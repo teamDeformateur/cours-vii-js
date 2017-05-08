8d9891f8c03a3521b3e5f7578cd493c166fafbdc
rplantefeve <romain.plantefeve@gmail.com>
2017-05-08 20:18:35 +0200

    Refactoring + improvements

---
+++ WebContent/exo-time-js/README.md
@@ -0,0 +1,22 @@
+# Timers :muscle:
+
+## Exo 1
+
+Une bombe va exploser... Il faut réagir vite !
+
+Simuler un compte à rebours. Si l'utilisateur appuie sur le bouton, le décompte s'arrête. Nous sommes sauvés !
+
+## Exo 2 : Enchères
+
+* On a un compte à rebours. On attend une seconde, et on décrémente, et on recommence.
+
+* Un form (input + button), à la soumission du form, on récupère
+le montant dans l'input.
+
+	- Si ce montant est supérieur à l'enchère en cours, alors
+	ça devient la nouvelle enchère en cours, et on réinitialise
+	le compteur à 10.
+
+	- Si ce montant n'est pas supérieur, il ne se passe rien.
+
+* Si le compteur arrive à 0, le formulaire disparait.
---
+++ WebContent/exo-time-js/js/exo1.js
@@ -0,0 +1,47 @@
+// Je récupère mon bouton
+var button = document.getElementById('stop');
+
+// le temps passe...
+function tictac(){
+	var temps = document.getElementById('timer');
+	var secondes = parseInt(temps.textContent,10);
+	secondes--;
+	temps.textContent = secondes;
+}
+
+//j'exécute la fonction tictac toutes les secondes
+var intervalID = setInterval(tictac, 1000);
+
+function writeOk() {
+  console.info('writeOk');
+
+  // Je récupère le div
+  var counter = document.getElementById('counter');
+
+  // J'écris dedans
+  counter.textContent = 'Trop tard.. BOUM !';
+  
+  // je désactive le bouton
+  button.style.visibility = "hidden";
+
+  // J'arrête l'interval
+  clearInterval(intervalID);
+}
+
+
+// J'exécute la fonction writeOk dans 3000 ms
+var timer = setTimeout(writeOk, 3000);
+
+// Stopper le timer
+function stopTimer() {
+  console.info('stopTimer');
+
+  // Avec clearTimeout, on peut arrêter le compte à rebours
+  clearTimeout(timer);
+  //J'arrête l'interval
+  clearInterval(intervalID);
+}
+
+//J'écoute le clic
+button.addEventListener('click', stopTimer);
+
