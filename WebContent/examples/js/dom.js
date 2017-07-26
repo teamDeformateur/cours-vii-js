baf63cd517a5a67eb2b9a26a940e068ac093589e
rplantefeve <romain.plantefeve@gmail.com>
2017-07-26 15:01:01 +0200

    Exemple d'évènement ajouté

--- WebContent/examples/js/dom.js
+++ WebContent/examples/js/dom.js
@@ -8,9 +8,23 @@ monParagraphe.textContent = "Youhou";
 // j'ajoute le noeud enfant monParagraphe au noeud parent div du DOM
 element.appendChild(monParagraphe);
 
+// I get the button
+var button = document.querySelector('#button1');
+// I add a click listener
+button.onclick = onClickListener;
 
-element = document.querySelector("#vousmavezditdedireid");
-element.innerHTML = "<p>Vous pouvez répéter la question ?</p>";
+var compteur = 0;
+
+// i declare the listener
+function onClickListener(event){
+	// i get a paragraph
+	let p = document.querySelector("#coordinates");
+	// i change the content of the paragraph
+	p.textContent = compteur++;
+}
+
+//element = document.querySelector("#vousmavezditdedireid");
+//element.innerHTML = "<p>Vous pouvez répéter la question ?</p>";
 
 let toutesLesDiv = document.querySelectorAll("div");
 
